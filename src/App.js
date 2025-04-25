2324a0acd2fe9bfbf1ebba71fc6ee782caa361df
arsBayone <sanjum@bayone.com>
2025-04-25 10:36:06 +0530

    update

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ function App() {
       <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
         Learn React
       </a>
-      <h5>addded new line</h5>
       <Counter />
     </div>
   );
